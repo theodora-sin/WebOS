524ecbda1411d563ad0b1f25a285cc1675ee8c4e
theodora-sin <theodrasin@gmail.com>
2026-07-21 16:57:09 +0100

    Update city.js

--- city.js
+++ city.js
@@ -244,7 +244,7 @@ const cities=[
             image:"studio.jpg",
             description:"Kinopio's Café (also known as Toadstool Cafe). It is a highly themed, quick-service restaurant designed to look like a giant mushroom, featuring dishes crafted by Chef Toad from Super Mario.",
             dish:"Chef's Special: Omelet Rice with Shrimp & Mushroom Cream Stew",
-            dishimage:"studio_food.jpg",
+            dishimage:"assets/studio_food.jpg",
             price:"$$, ¥2,000–3,000 per person",
             address:"2 Chome-1-33 Sakurajima, Konohana Ward, Osaka, 554-0031, Japan",
             map:"https://www.google.com/maps/place/Kinopio's+Cafe/@34.6676194,135.430082,17z/data=!4m15!1m8!3m7!1s0x6000e903643fc01d:0xc2316b38ad650124!2sKinopio's+Cafe!8m2!3d34.667881!4d135.4301919!10e9!16s%2Fg%2F11qyfq39v0!3m5!1s0x6000e903643fc01d:0xc2316b38ad650124!8m2!3d34.667881!4d135.4301919!16s%2Fg%2F11qyfq39v0?entry=ttu&g_ep=EgoyMDI2MDcxOS4wIKXMDSoASAFQAw%3D%3D"
@@ -252,10 +252,10 @@ const cities=[
         {
             id:"ramen",
             shop:"The Ramen War",
-            image:"war.jpg",
+            image:"assets/war.jpg",
             description:"A popular, moden Japanese ramen chain, it is known for its stylish and uniquely named bowls",
             dish:"Pistol",
-            dishimage:"war_food.jpg",
+            dishimage:"assets/war_food.jpg",
             price:"$,¥1,000–2,000 per person ",
             address:"There are many branches,please copy the shop name and search for your nearest branches.",
             map:"https://www.google.com/maps/search/The+Ramen+War/@34.7842711,132.3134146,6z/data=!3m1!4b1?entry=ttu&g_ep=EgoyMDI2MDcxOS4wIKXMDSoASAFQAw%3D%3D",
@@ -453,7 +453,7 @@ function detailedScreen(cityId, restaurantId) {
 
     const price = document.createElement("p");
     price.textContent = restaurant.price;
-    dishText.append(dishName, price);
+    dishText.append(dishLabel, dishName, price);
  
     dishBlock.append(dishImg, dishText);
     right.appendChild(dishBlock);
@@ -478,7 +478,7 @@ function detailedScreen(cityId, restaurantId) {
     mapimg.width= 24;
     
     map.className="maplink";
-    map.appendChild(img);
+    map.appendChild(mapimg);
     right.appendChild(map);
 
     const backButton=document.createElement("button");
@@ -497,4 +497,3 @@ function detailedScreen(cityId, restaurantId) {
 
 /*Screen loading*/
 document.addEventListener("DOMContentLoaded", startScreen);
-
